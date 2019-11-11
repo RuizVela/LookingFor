e686ce0167e6dcff87d203e851c8bd18563f8f3d
Raul <ruizvela271@gmail.com>
2019-11-11 11:32:01 +0100

    Clubs factory y seeders

--- Beauty/database/factories/ClubFactory.php
+++ Beauty/database/factories/ClubFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Club::class, function (Faker $faker) {
     return [
-        //
+        'name'=> $faker->text(20),
+        'description'=> $faker->text(100),
     ];
 });
--- Beauty/database/seeds/DatabaseSeeder.php
+++ Beauty/database/seeds/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             GamesTableSeeder::class,
+            ClubsTableSeeder::class,
             GroupsTableSeeder::class,
             OffersTableSeeder::class,
             AnswersTableSeeder::class,
