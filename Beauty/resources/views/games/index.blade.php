6169b30e2f5e0291672d430c36d66510a2c9cac6
Raul <ruizvela271@gmail.com>
2019-11-11 16:35:09 +0100

    vista de index

--- Beauty/resources/views/games/index.blade.php
+++ Beauty/resources/views/games/index.blade.php
@@ -1,1 +1,9 @@
-hola
+@foreach ($games as $game)
+    {{$game->name}}<br>
+    <form action="game/{{$game->id}}/edit"
+        method="GET"> 
+        <input type="submit" value="Editar">
+    </form>
+    
+@endforeach
+
