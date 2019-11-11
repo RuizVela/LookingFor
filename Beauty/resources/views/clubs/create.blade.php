9d884961c80711615c5703fe4705575126030fd9
Nellay <nellay2020@gmail.com>
2019-11-11 14:13:02 +0100

    formulario create club

--- Beauty/resources/views/clubs/create.blade.php
+++ Beauty/resources/views/clubs/create.blade.php
@@ -1,1 +1,11 @@
-hola
+<form method="POST" action="club">
+    @csrf
+    <label>Nombre del club:</label>
+    <input type="text" name="name">
+    <br>
+    <label>Descripción:</label>
+    <input type="text" name="description">
+    <br>
+<input type="submit" value="Crear">
+</form>
+
