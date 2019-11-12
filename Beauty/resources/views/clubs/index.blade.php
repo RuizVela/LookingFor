09968c312dfe2efae7e215af2aa86c2e0a8f3e63
Raul <ruizvela271@gmail.com>
2019-11-12 09:41:49 +0100

    vista index club hecha

--- Beauty/resources/views/clubs/index.blade.php
+++ Beauty/resources/views/clubs/index.blade.php
@@ -1,1 +1,8 @@
-hola
+@foreach ($clubs as $club)
+    {{$club->name}}<br>
+    <form action="/game/{{$club->id}}/edit"
+        method="GET"> 
+        <input type="submit" value="Editar">
+    </form>
+    
+@endforeach
