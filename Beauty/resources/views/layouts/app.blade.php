f9c5150575a1e4d92c55bc2017a9c6af8fd404df
Raul <ruizvela271@gmail.com>
2020-01-07 09:06:04 +0100

    cambios en layout

--- Beauty/resources/views/layouts/app.blade.php
+++ Beauty/resources/views/layouts/app.blade.php
@@ -1,13 +1,11 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,22 +19,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container">                    
+             
+                    <h1>LookingFor</h1>
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
---
+++ Beauty/resources/views/layouts/master.blade.php
@@ -0,0 +1,5 @@
+
+
+        <div class="container" style="background-color:red">
+            @yield('content')
+        </div>
