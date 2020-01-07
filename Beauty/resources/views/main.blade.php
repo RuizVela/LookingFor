4e53a602e5f6383b440487401c663655254640a1
Raul <ruizvela271@gmail.com>
2020-01-07 11:31:43 +0100

    Botones "mostrar clubs" y "mostrar usuarios" en el layout app

--- Beauty/resources/views/main.blade.php
+++ Beauty/resources/views/main.blade.php
@@ -11,12 +11,7 @@
     }
     </style>
 <body>    
-    <form method="GET" action="/profile">
-        <input type="submit" value="Mostrar Perfiles">
-    </form>
-    <form method="GET" action="/club">
-        <input type="submit" value="Mostrar Clubs">
-    </form>
+
     @include('games.index')
     
     
