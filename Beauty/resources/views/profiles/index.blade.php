f75fc134e6f66674d8ffb443836db0222ec577a2
Raul <ruizvela271@gmail.com>
2020-01-07 15:11:50 +0100

    archivo general.scss para todos los estilos generales de la web

--- Beauty/resources/views/profiles/index.blade.php
+++ Beauty/resources/views/profiles/index.blade.php
@@ -1,9 +1,12 @@
+@extends('layouts.app')
+@section('content')
 <form method="GET" action="/main">
     <input type="submit" value="Página principal">
 </form>
 Lista de Perfiles:
 @foreach ($profiles as $profile)
-    <li> {{$profile->name}}<form method="GET" action="/profile/{{$profile->id}}">
-        <input type="submit" value="Mostrar">
-    </form></li>
+<li> {{$profile->name}}<form method="GET" action="/profile/{{$profile->id}}">
+    <input type="submit" value="Mostrar">
+</form></li>
 @endforeach
+@endsection
