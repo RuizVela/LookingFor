e260a64202e410a88a8c7a633b6f542505c5824d
Raul <ruizvela271@gmail.com>
2020-01-09 12:13:02 +0100

    comentados tests de ejemplo para que no se activen

--- Beauty/tests/Unit/ExampleTest.php
+++ Beauty/tests/Unit/ExampleTest.php
@@ -5,15 +5,15 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
-}
+// class ExampleTest extends TestCase
+// {
+//     /**
+//      * A basic test example.
+//      *
+//      * @return void
+//      */
+//     public function testBasicTest()
+//     {
+//         $this->assertTrue(true);
+//     }
+// }
